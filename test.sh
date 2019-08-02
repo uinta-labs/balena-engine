eaed521246882ce970304a14266e478cb8b32a78
Robert Günzler <r@gnzler.io>
2019-08-02 18:35:06 +0200

    wip: run integration tests too
    Signed-off-by: Robert Günzler <robertg@balena.io>

--- test.sh
+++ test.sh
@@ -4,4 +4,7 @@ set -ex
 
 hack/dind hack/test/unit
 
-# hack/dind hack/make.sh dynbinary test-integration
+# prevent legacy test suites from running. they take a very long time
+sed -i -e '/run_test_integration_legacy_suites$/ s/^/#/' hack/make/.integration-test-helpers
+
+hack/dind hack/make.sh dynbinary test-integration
